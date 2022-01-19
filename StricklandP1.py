a80b3445a8f2c47481deb45e7660c19698fe8215
jamesonstrickland <jamesonleestrickland@gmail.com>
2022-01-18 22:24:50 -0700

    Create StricklandP1.py

---
+++ StricklandP1.py
@@ -0,0 +1,35 @@
+# Strickland
+# Programmer: Jameson Strickland
+# EMail: jstrickland4@cnm.edu
+# Purpose: provides user capability to calculate volume and surface area of a pyramid.
+# Python version: 3.7.8
+
+# Length of the base:  a
+# Height of the pyramid:  h
+# Volume = (((a^2)(h))/3)
+# Slant height, s  = sqrt(h^2 + (a/2)^2)  This is the Pythagorean Theorem part.
+# Area of one pyramid side = s*a/2
+
+import math
+
+print("When prompted, please input values.")
+
+
+a = float(input("Enter a value for the length of the base of the pyramid (in feet): "))
+
+h = float(input("Enter a value for the height of the pyramid (in feet): "))
+
+s = float(math.sqrt((h**2) + ((a/2)**2)))
+    
+volume = (a**2)*(h/3)
+
+surfacearea = (((s)*(a/2))*4)
+
+print('Base: ',a)
+print('Height: ',h)
+print('Volume of Pyramid: ',"%.3f" % volume)
+print('Surface Area of Pyramid (without base): ',"%.3f" % surfacearea)
+
+
+
+
